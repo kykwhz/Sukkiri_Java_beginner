3ff7cb8d03b9328b9764920336b6d08cb6eabb23
kykwhz <kykwhz@gmail.com>
2021-08-13 18:09:10 +0900

    14.2.2 Objectクラスの存在価値

---
+++ スッキリわかるJava入門/src/chapter14/object/Code14_02.java
@@ -0,0 +1,12 @@
+package chapter14.object;
+
+public class Code14_02 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Object o1 = new Empty();
+		Object o2 = new Hero();
+		Object o3 = "こんにちは";
+	}
+
+}
---
+++ スッキリわかるJava入門/src/chapter14/object/Empty.java
@@ -0,0 +1,5 @@
+package chapter14.object;
+
+public class Empty {
+
+}
