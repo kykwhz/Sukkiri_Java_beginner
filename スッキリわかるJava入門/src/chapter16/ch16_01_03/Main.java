63eca39a27aafdfb82fe49d98d2e078c1dec136c
kykwhz <kykwhz@gmail.com>
2021-08-17 23:14:19 +0900

    「コード16-1　ラッパークラスをArrayListに格納する」を追加。

---
+++ スッキリわかるJava入門/src/chapter16/ch16_01_03/Main.java
@@ -0,0 +1,18 @@
+package chapter16.ch16_01_03;
+
+import java.util.ArrayList;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		ArrayList<Integer> points = new ArrayList<Integer>();
+		points.add(10);
+		points.add(80);
+		points.add(75);
+		for (Integer integer : points) {
+			System.out.println(integer);
+		}
+	}
+
+}
